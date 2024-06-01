65f9dd353aef252c5b7b22aa316a34c0ecdbe776
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2024-05-31 22:25:06 -0500

    create last version & dev version

--- DataCompressor/Installer/add_path.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-# Ruta al directorio raíz donde se ejecuta el archivo
-root_path = os.path.abspath(os.path.dirname(sys.argv[0]))
-
-# Agregar la carpeta raíz al PATH del sistema
-os.environ['PATH'] = root_path + os.pathsep + os.environ.get('PATH', '')
